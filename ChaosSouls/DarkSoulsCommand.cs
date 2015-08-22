5f5c92c046eba58c54ec0a37aaef85145981652e
Andrew Crute <abc8a@virginia.edu>
2015-08-22 03:00:50 -0400

    kill unneeded references
    InputSimulator didn't work, and SharpDX wasn't what we needed

--- ChaosSouls/DarkSoulsCommand.cs
+++ ChaosSouls/DarkSoulsCommand.cs
@@ -5,8 +5,6 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using WindowsInput;
-using WindowsInput.Native;
 
 namespace ChaosSouls
 {
